2592d8198d41a2364d8f0498a03656a5a78545d1
jasleen <jasleenkaur2526@gmail.com>
2023-08-13 13:19:57 +0800

    login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; 
 
-function App() {
+function YourComponent() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="centered-box">
+      <div className="login-box">
+        <img src="/RCEWA_Logo.png" alt="Logo" className="logo-image" />
+        <h3 className="login-heading">Log In</h3>
+        <form className="login-form" method="post">
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input type="text" name="username" className="input-field" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" className="input-field" />
+          </div>
+          <p className="signup-link">No account yet? Sign up <a href="/signup" style={{ color: 'white' }}>HERE</a></p>
+          <input type="submit" value="Submit" className="submit-button" />
+        </form>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default YourComponent;
